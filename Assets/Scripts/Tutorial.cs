be03c9e5e11ab99c021df59c5de5af23e4e9c9fb
Ayoub <ayoub.azzouzi@hes-so.ch>
2025-06-07 12:57:18 +0200

    tutorial script

--- Assets/Scripts/Tutorial.cs
+++ Assets/Scripts/Tutorial.cs
@@ -3,9 +3,9 @@ using UnityEngine;
 public class Tutorial : MonoBehaviour
 {
     [Header("References")]
-    public GameObject cloud;
+    public GameObject[] cloudFields; // 9 clouds to be assigned in Inspector
     public GameObject hand;
-    public GameObject[] handAnimations; // Assign ZaHandoMT objects here
+    public GameObject[] handAnimations;
 
     [Header("Sounds")]
     public AudioSource pushSound;
@@ -20,32 +20,41 @@ public class Tutorial : MonoBehaviour
     public Color glowColor = Color.cyan;
     public float glowIntensity = 3f;
 
-    private Vector3 startPosition;
+    private Vector3[,] startPositions = new Vector3[3, 3];
+    private GameObject[,] clouds = new GameObject[3, 3];
+
     private float timeElapsed;
     private float lastZ;
     private bool movingForward = true;
 
-    private GravityToPlayer gravityScript;
+    private GravityToPlayer[,] gravityScripts = new GravityToPlayer[3, 3];
     private Material handMaterial;
     private Color baseEmission;
+
     private bool tutorialStarted = false;
     private bool tutorialFinished = false;
 
     void Start()
     {
-        if (!cloud || !hand)
+        if (cloudFields.Length != 9 || !hand)
         {
-            Debug.LogError("Assign both cloud and hand.");
+            Debug.LogError("Assign exactly 9 cloud objects and the hand.");
             enabled = false;
             return;
         }
 
-        startPosition = cloud.transform.position;
-        cloud.transform.position = startPosition;
-        hand.SetActive(false);
+        // Setup 3x3 grid of clouds
+        for (int i = 0; i < 9; i++)
+        {
+            int x = i % 3;
+            int y = i / 3;
+            clouds[x, y] = cloudFields[i];
+            startPositions[x, y] = cloudFields[i].transform.position;
+            gravityScripts[x, y] = cloudFields[i].GetComponent<GravityToPlayer>();
+            if (gravityScripts[x, y]) gravityScripts[x, y].enabled = false;
+        }
 
-        gravityScript = cloud.GetComponent<GravityToPlayer>();
-        if (gravityScript) gravityScript.enabled = false;
+        hand.SetActive(false);
 
         // Mute all audio globally at start
         AudioListener.volume = 0f;
@@ -99,8 +108,19 @@ public class Tutorial : MonoBehaviour
         {
             float phase = Mathf.Sin((timeElapsed / duration) * oscillations * 2 * Mathf.PI);
             float zOffset = phase * (distance / 2);
-            cloud.transform.position = startPosition + new Vector3(0, 0, zOffset);
 
+            for (int x = 0; x < 3; x++)
+            {
+                for (int y = 0; y < 3; y++)
+                {
+                    if (clouds[x, y])
+                    {
+                        clouds[x, y].transform.position = startPositions[x, y] + new Vector3(0, 0, zOffset);
+                    }
+                }
+            }
+
+            // Sound logic
             if (zOffset > lastZ && !movingForward)
             {
                 movingForward = true;
@@ -116,8 +136,18 @@ public class Tutorial : MonoBehaviour
         }
         else
         {
-            cloud.transform.position = startPosition;
-            if (gravityScript) gravityScript.enabled = true;
+            // End tutorial
+            for (int x = 0; x < 3; x++)
+            {
+                for (int y = 0; y < 3; y++)
+                {
+                    if (clouds[x, y])
+                    {
+                        clouds[x, y].transform.position = startPositions[x, y];
+                        if (gravityScripts[x, y]) gravityScripts[x, y].enabled = true;
+                    }
+                }
+            }
 
             hand.SetActive(false);
             tutorialFinished = true;
