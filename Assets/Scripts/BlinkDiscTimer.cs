d044555e9dc5cf8491ad03565266d20856f25504
KMWolff <karina.wolff@hes-so.ch>
2025-06-07 17:19:32 +0200

    sfx Disc blink

--- Assets/Scripts/BlinkDiscTimer.cs
+++ Assets/Scripts/BlinkDiscTimer.cs
@@ -2,16 +2,20 @@ using UnityEngine;
 
 public class BlinkOnTimer : MonoBehaviour
 {
-    public DiscDestroy discDestroy; // Reference to the script with the timer
+    public DiscDestroy discDestroy;
     public float blinkSpeed = 0.5f;
+    public AudioSource blinkAudio;      // Sound to play during blink
+    public AudioSource normalAudio;     // Ongoing sound to stop during blink
 
     private Renderer rend;
     private float blinkTimer;
+    private bool isBlinking = false;
 
     void Start()
     {
         rend = GetComponent<Renderer>();
-        // Find gameObject with the tag "Disc"
+
+        // Find disc with tag
         GameObject discObject = GameObject.FindGameObjectWithTag("Disc");
         if (discObject != null)
         {
@@ -21,7 +25,6 @@ public class BlinkOnTimer : MonoBehaviour
         {
             Debug.LogError("No GameObject with tag 'Disc' found.");
         }
-
     }
 
     void Update()
@@ -30,6 +33,13 @@ public class BlinkOnTimer : MonoBehaviour
 
         if (time <= 10f && time > 0f)
         {
+            if (!isBlinking)
+            {
+                isBlinking = true;
+                if (normalAudio && normalAudio.isPlaying) normalAudio.Stop();
+                if (blinkAudio && !blinkAudio.isPlaying) blinkAudio.Play();
+            }
+
             blinkTimer += Time.deltaTime;
             if (blinkTimer >= blinkSpeed)
             {
@@ -39,6 +49,13 @@ public class BlinkOnTimer : MonoBehaviour
         }
         else
         {
+            if (isBlinking)
+            {
+                isBlinking = false;
+                if (blinkAudio && blinkAudio.isPlaying) blinkAudio.Stop();
+                if (normalAudio && !normalAudio.isPlaying) normalAudio.Play();
+            }
+
             rend.enabled = true;
             blinkTimer = 0f;
         }
